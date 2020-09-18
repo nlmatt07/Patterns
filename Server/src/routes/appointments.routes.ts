0ba855004f69b8053f2ed6f0c569fb67e33a88eb
Mateus Nadal <nlmatt07@hotmail.com>
2020-09-18 14:46:50 -0300

    Registration of Appointments

--- Server/src/routes/appointments.routes.ts
+++ Server/src/routes/appointments.routes.ts
@@ -1,3 +1,22 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
-const routes = Router();
+const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
